7425849ccd03f34e1d271749f025c494bc09ddca
Carlos Gonzalez <cgonzalezrodriguez@vistaprint.com>
2017-06-06 10:57:47 +0200

    Passing second basic test

--- CoduranceTraining.Tests/MarsRoverShould.cs
+++ CoduranceTraining.Tests/MarsRoverShould.cs
@@ -5,18 +5,24 @@ namespace CoduranceTraining.Tests
     [TestFixture]
     public class MarsRoverShould
     {
+        private MarsRover _marsRover;
+
+        [SetUp]
+        public void Setup()
+        {
+            _marsRover = new MarsRover();
+        }
+
         [Test]
         public void return_initial_position_when_no_commands_provided()
-        {
-            var marsRover = new MarsRover();
-            Assert.AreEqual("0,0,N", marsRover.Move(string.Empty));
+        {         
+            Assert.AreEqual("0,0,N", _marsRover.Move(string.Empty));
         }
         
         [Test]
         public void move_one_position_in_the_current_direction_when_one_move_command_provided()
-        {
-            var marsRover = new MarsRover();
-            Assert.AreEqual("0,1,N", marsRover.Move("M"));
+        {    
+            Assert.AreEqual("0,1,N", _marsRover.Move("M"));
         }
 
 
--- CoduranceTraining/MarsRover.cs
+++ CoduranceTraining/MarsRover.cs
@@ -2,8 +2,13 @@
 {
     public class MarsRover
     {
-        public string Move(string empty)
+        public string Move(string command)
         {
+            if (command == "M")
+            {
+                return "0,1,N"; 
+            }
+
             return "0,0,N";
         }
     }
