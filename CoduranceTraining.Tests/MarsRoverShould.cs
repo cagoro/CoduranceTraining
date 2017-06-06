f1e5668b195adad4a1922cffdb81d8ecf348bcde
Carlos Gonzalez <cgonzalezrodriguez@vistaprint.com>
2017-06-06 10:51:48 +0200

    Second test testing basic movement

--- CoduranceTraining.Tests/MarsRoverShould.cs
+++ CoduranceTraining.Tests/MarsRoverShould.cs
@@ -11,5 +11,15 @@ namespace CoduranceTraining.Tests
             var marsRover = new MarsRover();
             Assert.AreEqual("0,0,N", marsRover.Move(string.Empty));
         }
+        
+        [Test]
+        public void move_one_position_in_the_current_direction_when_one_move_command_provided()
+        {
+            var marsRover = new MarsRover();
+            Assert.AreEqual("0,1,N", marsRover.Move("M"));
+        }
+
+
+
     }
 }
